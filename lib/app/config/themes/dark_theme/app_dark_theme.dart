feee620a37d8f3fe4020da7f579c1b690a15bf52
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:53:11 -1100

    Initialize listTile ThemeData for dark ThemeData

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
+import 'package:music_app/app/config/themes/dark_theme/app_list_tile_dark_theme.dart';
 
 class AppDarkTheme {
   AppDarkTheme._();
   static ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: AppLDarkModeColors.blackColor,
+    listTileTheme: AppListTileDarkTheme.listTileTheme,
   );
 }

63d97352739ef5d4eed879c0313b481f164d57c0
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:52:11 -1100

    Create default style for list tile on dark mode

---
+++ lib/app/config/themes/dark_theme/app_list_tile_dark_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/app/config/app_constants.dart';
+import 'package:music_app/app/config/app_paddings.dart';
+import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
+
+class AppListTileDarkTheme {
+  AppListTileDarkTheme._();
+  static const ListTileThemeData listTileTheme = ListTileThemeData(
+    iconColor: AppLDarkModeColors.whiteColor,
+    contentPadding: AppPaddings.listTileContentPadding,
+    shape: RoundedRectangleBorder(
+      borderRadius: AppConstants.normalBorderRadius,
+      side: BorderSide.none,
+    ),
+    horizontalTitleGap: 0,
+  );
+}

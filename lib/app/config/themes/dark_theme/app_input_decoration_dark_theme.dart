eb33e24508bb1a4baf372ee8a79eef33b6a2a282
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:11:59 -1100

    Define input decoration on dark theme

---
+++ lib/app/config/themes/dark_theme/app_input_decoration_dark_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/app/config/app_constants.dart';
+import 'package:music_app/app/config/app_paddings.dart';
+import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
+
+class AppInputDecorationDarkTheme {
+  AppInputDecorationDarkTheme._();
+  static const InputDecorationTheme inputDecorationTheme = InputDecorationTheme(
+    contentPadding: AppPaddings.inputContentPadding,
+    border: OutlineInputBorder(
+      borderRadius: AppConstants.normalBorderRadius,
+      borderSide: BorderSide(
+        color: AppDarkModeColors.greyColor,
+        width: 1.0,
+      ),
+    ),
+    hintStyle: TextStyle(
+      fontFamily: 'Segoe',
+      fontSize: 12.0,
+      color: AppDarkModeColors.darkGreyColor,
+    ),
+  );
+}

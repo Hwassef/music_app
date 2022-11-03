13cc9214ffead54f3f1e77907863adb73469ad6e
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:03:05 -1100

    Add input content padding constant

--- lib/app/config/app_paddings.dart
+++ lib/app/config/app_paddings.dart
@@ -2,7 +2,13 @@ import 'package:flutter/cupertino.dart';
 
 class AppPaddings {
   AppPaddings._();
+  static const double smallValue = 12.0;
   static const double normalValue = 16.0;
   static const double mediumValue = 22.0;
   static const EdgeInsets listTileContentPadding = EdgeInsets.symmetric(horizontal: normalValue, vertical: mediumValue);
+  static const EdgeInsets inputContentPadding = EdgeInsets.only(
+    left: mediumValue,
+    top: smallValue,
+    bottom: smallValue,
+  );
 }

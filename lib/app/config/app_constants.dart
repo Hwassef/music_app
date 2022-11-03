c792d56d8a3cdaceda02c66cae6d99e1b5f2828e
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:49:46 -1100

    Add normal border radius constant

---
+++ lib/app/config/app_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  AppConstants._();
+  static const double normalValue = 12.0;
+  static const Radius normalRadius = Radius.circular(normalValue);
+  static const BorderRadius normalBorderRadius = BorderRadius.all(normalRadius);
+}

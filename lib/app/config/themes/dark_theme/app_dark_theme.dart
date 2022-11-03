9769be5da60226f3fa5c47dde446ba8c2a85148c
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:31:48 -1100

    Initialize bottom navigation bar ThemeData for dark ThemeData

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 import 'package:music_app/app/config/themes/dark_theme/app_list_tile_dark_theme.dart';
 
+import 'app_bottom_navigation_bar_dark_theme.dart';
 import 'app_input_decoration_dark_theme.dart';
 
 class AppDarkTheme {
@@ -10,5 +11,6 @@ class AppDarkTheme {
     scaffoldBackgroundColor: AppDarkModeColors.blackColor,
     listTileTheme: AppListTileDarkTheme.listTileTheme,
     inputDecorationTheme: AppInputDecorationDarkTheme.inputDecorationTheme,
+    bottomNavigationBarTheme: AppBottomNavigationBarDarkTheme.bottomNavigationBarThemeData,
   );
 }

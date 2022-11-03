122210e3cc3a4ea91350cb43b7e487a08ebf9dba
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:55:35 -1100

    Initialize bottom sheet ThemeData on light mode

--- lib/app/config/themes/light_theme/app_light_theme.dart
+++ lib/app/config/themes/light_theme/app_light_theme.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 
 import 'app_bottom_navigation_bar_light_theme.dart';
+import 'app_bottom_sheet_light_theme.dart';
 
 class AppLightTheme {
   AppLightTheme._();
   static ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: AppDarkModeColors.whiteColor,
     bottomNavigationBarTheme: AppBottomNavigationBarLightTheme.bottomNavigationBarThemeData,
+    bottomSheetTheme: AppBottomSheetLightTheme.bottomSheetThemeData,
   );
 }

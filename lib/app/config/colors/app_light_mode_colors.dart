01387a8e8a891b67044ed6214a074dc6c15e46f6
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:17:43 -1100

    Add red color hex code

--- lib/app/config/colors/app_light_mode_colors.dart
+++ lib/app/config/colors/app_light_mode_colors.dart
@@ -6,4 +6,5 @@ class AppLightModeColors {
   static const Color greyColor = Color(0xFF868686);
   static const Color darkGreyColor = Color(0xFF707070);
   static const Color lightBlackColor = Color(0xFF242424);
+  static const Color redColor = Color(0xFFFF0203);
 }

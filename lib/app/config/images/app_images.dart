f303e147e76f5760a6d6b47f3740e3a7fdd35c9d
Hwassef <hassine.wassef@outlook.com>
2022-11-03 11:19:47 -1100

    Add paths to asset images

---
+++ lib/app/config/images/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  AppImages._();
+  static const imagesFolderPath = 'assets/images';
+  static const iconsFolderPath = 'assets/icons';
+  static const favoriteIcon = '$iconsFolderPath/favorite_icon.svg';
+  static const homeIcon = '$iconsFolderPath/home_icon.svg';
+  static const playlistIcon = '$iconsFolderPath/playlist_icon.svg';
+  static const profileIcon = '$iconsFolderPath/profile_icon.svg';
+}

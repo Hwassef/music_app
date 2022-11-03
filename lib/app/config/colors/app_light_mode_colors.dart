5f967a54fc5dd8657716d29733ce74029dd269ec
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:25:13 -1100

    Add white color hex code

--- lib/app/config/colors/app_light_mode_colors.dart
+++ lib/app/config/colors/app_light_mode_colors.dart
@@ -2,10 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppLightModeColors {
   AppLightModeColors._();
-  static const Color blackColor = Color(0xFF0E0E0E);
-  static const Color greyColor = Color(0xFF868686);
-  static const Color darkGreyColor = Color(0xFF707070);
-  static const Color lightBlackColor = Color(0xFF242424);
-  static const Color redColor = Color(0xFFFF0203);
   static const Color whiteColor = Color(0xFFFFFFFF);
 }

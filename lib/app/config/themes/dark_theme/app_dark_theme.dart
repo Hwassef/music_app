5f3b9203ece03dcdf96cea33737e2653dfaa2c0d
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:38:20 -1100

    Set default scaffold background color to black in dark mode

---
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
+
+class AppDarkTheme {
+  AppDarkTheme._();
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: AppLDarkModeColors.blackColor,
+  );
+}

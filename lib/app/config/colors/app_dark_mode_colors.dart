45e1f0b20f1444ad63e2494b5ea4f3d06987dcdd
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:06:43 -1100

    Rename class

--- lib/app/config/colors/app_dark_mode_colors.dart
+++ lib/app/config/colors/app_dark_mode_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AppLDarkModeColors {
-  AppLDarkModeColors._();
+class AppDarkModeColors {
+  AppDarkModeColors._();
   static const Color blackColor = Color(0xFF0E0E0E);
   static const Color greyColor = Color(0xFF868686);
   static const Color darkGreyColor = Color(0xFF707070);

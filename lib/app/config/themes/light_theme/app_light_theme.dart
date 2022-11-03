77eae4ff696d69490a4d4b6cbda695bd6419509a
Hwassef <hassine.wassef@outlook.com>
2022-11-03 11:00:07 -1100

    Initialize list tile ThemeData on light mode

--- lib/app/config/themes/light_theme/app_light_theme.dart
+++ lib/app/config/themes/light_theme/app_light_theme.dart
@@ -4,6 +4,7 @@ import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 import 'app_bottom_navigation_bar_light_theme.dart';
 import 'app_bottom_sheet_light_theme.dart';
 import 'app_input_decoration_light_theme.dart';
+import 'app_list_tile_light_theme.dart';
 
 class AppLightTheme {
   AppLightTheme._();
@@ -12,5 +13,6 @@ class AppLightTheme {
     bottomNavigationBarTheme: AppBottomNavigationBarLightTheme.bottomNavigationBarThemeData,
     bottomSheetTheme: AppBottomSheetLightTheme.bottomSheetThemeData,
     inputDecorationTheme: AppInputDecorationLightTheme.inputDecorationTheme,
+    listTileTheme: AppListTileLightTheme.listTileTheme,
   );
 }

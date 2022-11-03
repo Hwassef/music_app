798fce3b622381841c0566b21895747785d21cd7
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:46:14 -1100

    Add listTile content padding constant

---
+++ lib/app/config/app_paddings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppPaddings {
+  AppPaddings._();
+  static const double normalValue = 16.0;
+  static const double mediumValue = 22.0;
+  static const EdgeInsets listTileContentPadding = EdgeInsets.symmetric(horizontal: normalValue, vertical: mediumValue);
+}

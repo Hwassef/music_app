d5302b32a68f8ad613c6a8f4feccf0eb8c9139b9
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:53:13 -1100

    Inialize bottom navigation bar ThemeData on light mode

--- lib/app/config/themes/light_theme/app_light_theme.dart
+++ lib/app/config/themes/light_theme/app_light_theme.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 
+import 'app_bottom_navigation_bar_light_theme.dart';
+
 class AppLightTheme {
   AppLightTheme._();
   static ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: AppDarkModeColors.whiteColor,
+    bottomNavigationBarTheme: AppBottomNavigationBarLightTheme.bottomNavigationBarThemeData,
   );
 }

4cd19b273d7f67ce8b44b2f77e2e93b5124f2993
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:48:50 -1100

    Initialize modalBottomSheetThemeData on dark mode

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -3,6 +3,7 @@ import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 import 'package:music_app/app/config/themes/dark_theme/app_list_tile_dark_theme.dart';
 
 import 'app_bottom_navigation_bar_dark_theme.dart';
+import 'app_bottom_sheet_dark_theme.dart';
 import 'app_input_decoration_dark_theme.dart';
 
 class AppDarkTheme {
@@ -12,5 +13,6 @@ class AppDarkTheme {
     listTileTheme: AppListTileDarkTheme.listTileTheme,
     inputDecorationTheme: AppInputDecorationDarkTheme.inputDecorationTheme,
     bottomNavigationBarTheme: AppBottomNavigationBarDarkTheme.bottomNavigationBarThemeData,
+    bottomSheetTheme: AppBottomSheetDarkTheme.bottomSheetThemeData,
   );
 }

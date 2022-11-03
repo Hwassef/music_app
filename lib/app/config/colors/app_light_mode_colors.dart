c133fd249b52d22b5531aed6e9c1b3256c8fcb60
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:27:04 -1100

    Add light black color hex code

--- lib/app/config/colors/app_light_mode_colors.dart
+++ lib/app/config/colors/app_light_mode_colors.dart
@@ -4,4 +4,5 @@ class AppLightModeColors {
   AppLightModeColors._();
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color darkGreyColor = Color(0xFF868686);
+  static const Color lightBlackColor = Color(0xFF0E0E0E);
 }

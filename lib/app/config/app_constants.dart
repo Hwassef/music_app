449c2547b152d26ed8827d5a20eb8d9bbc2dea1c
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:46:03 -1100

    Add top horizontal border radius constant

--- lib/app/config/app_constants.dart
+++ lib/app/config/app_constants.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class AppConstants {
   AppConstants._();
   static const double normalValue = 12.0;
+  static const double largeValue = 24.0;
   static const Radius normalRadius = Radius.circular(normalValue);
+  static const Radius largeRadius = Radius.circular(largeValue);
   static const BorderRadius normalBorderRadius = BorderRadius.all(normalRadius);
+  static const BorderRadius topHorizontallargeBorderRadius = BorderRadius.only(
+    topRight: largeRadius,
+    topLeft: largeRadius,
+  );
 }

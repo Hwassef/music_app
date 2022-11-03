c45e9c12e6b72adc6535d4a05d1749a3ff7b135a
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:12:24 -1100

    Initialize input decoration ThemeData for dark ThemeData

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 import 'package:music_app/app/config/themes/dark_theme/app_list_tile_dark_theme.dart';
 
+import 'app_input_decoration_dark_theme.dart';
+
 class AppDarkTheme {
   AppDarkTheme._();
   static ThemeData darkTheme = ThemeData(
-    scaffoldBackgroundColor: AppLDarkModeColors.blackColor,
+    scaffoldBackgroundColor: AppDarkModeColors.blackColor,
     listTileTheme: AppListTileDarkTheme.listTileTheme,
+    inputDecorationTheme: AppInputDecorationDarkTheme.inputDecorationTheme,
   );
 }

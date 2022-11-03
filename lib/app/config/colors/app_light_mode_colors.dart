b0f54839a5fc8eed784dd95225e67ff05f809510
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:25:58 -1100

    Add dark grey color hex code

--- lib/app/config/colors/app_light_mode_colors.dart
+++ lib/app/config/colors/app_light_mode_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppLightModeColors {
   AppLightModeColors._();
   static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color darkGreyColor = Color(0xFF868686);
 }

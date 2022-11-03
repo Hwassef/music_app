ed8b35343b465bd095a767f47ff72b2f54d8f875
Hwassef <hassine.wassef@outlook.com>
2022-11-03 09:32:12 -1100

    Add red color hex code

--- lib/app/config/colors/app_light_mode_colors.dart
+++ lib/app/config/colors/app_light_mode_colors.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class AppLightModeColors {
   AppLightModeColors._();
   static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color lightGreyColor = Color(0xFFF5F5F5);
   static const Color greyColor = Color(0xFF868686);
   static const Color darkGreyColor = Color(0xFF707070);
   static const Color lightBlackColor = Color(0xFF0E0E0E);
-  static const Color lightGreyColor = Color(0xFFF5F5F5);
+  static const Color redColor = Color(0xFFFF0203);
 }

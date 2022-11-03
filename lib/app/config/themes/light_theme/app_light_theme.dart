e67d9a02021f701ddd964d89886d1c4f101019fc
Hwassef <hassine.wassef@outlook.com>
2022-11-03 10:58:05 -1100

    Initialize input decoration theme on light mode

--- lib/app/config/themes/light_theme/app_light_theme.dart
+++ lib/app/config/themes/light_theme/app_light_theme.dart
@@ -3,6 +3,7 @@ import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 
 import 'app_bottom_navigation_bar_light_theme.dart';
 import 'app_bottom_sheet_light_theme.dart';
+import 'app_input_decoration_light_theme.dart';
 
 class AppLightTheme {
   AppLightTheme._();
@@ -10,5 +11,6 @@ class AppLightTheme {
     scaffoldBackgroundColor: AppDarkModeColors.whiteColor,
     bottomNavigationBarTheme: AppBottomNavigationBarLightTheme.bottomNavigationBarThemeData,
     bottomSheetTheme: AppBottomSheetLightTheme.bottomSheetThemeData,
+    inputDecorationTheme: AppInputDecorationLightTheme.inputDecorationTheme,
   );
 }

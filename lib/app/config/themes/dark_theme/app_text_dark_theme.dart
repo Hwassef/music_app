b78efc3b584787c3d054a5104e89531150a74b4c
Hwassef <hassine.wassef@outlook.com>
2022-11-03 15:38:10 -1100

    Define text theme on dark mode

---
+++ lib/app/config/themes/dark_theme/app_text_dark_theme.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+
+import '../../colors/app_dark_mode_colors.dart';
+
+class AppTexDarkTheme {
+  AppTexDarkTheme._();
+  static const String _fontFamily = 'Lato';
+
+  ///Font weights
+  static const FontWeight _light = FontWeight.w300;
+  static const FontWeight _regular = FontWeight.w400;
+  static const FontWeight _medium = FontWeight.w500;
+  static const FontWeight _semiBold = FontWeight.w600;
+  static const FontWeight _bold = FontWeight.w700;
+
+  static TextTheme textTheme = TextTheme(
+    titleSmall: _titleSmall,
+    titleMedium: _titleMedium,
+    titleLarge: _titleLarge,
+    subtitle2: _subtitle2,
+    subtitle1: _subtitle1,
+    overline: _overline,
+    labelSmall: _labelSmall,
+    labelMedium: _labelMedium,
+    labelLarge: _labelLarge,
+    headlineSmall: _headlineSmall,
+    headlineMedium: _headlineMedium,
+    headlineLarge: _headlineLarge,
+    headline6: _headline6,
+    headline5: _headline5,
+    headline4: _headline4,
+    headline3: _headline3,
+    headline2: _headline2,
+    headline1: _headline1,
+    displaySmall: _displaySmall,
+    displayMedium: _displayMedium,
+    displayLarge: _displayLarge,
+    caption: _caption,
+    button: _button,
+    bodyText2: _bodyText2,
+    bodyText1: _bodyText1,
+    bodySmall: _bodySmall,
+    bodyMedium: _bodyMedium,
+    bodyLarge: _bodyLarge,
+  );
+  static const TextStyle _titleSmall = TextStyle(
+    fontWeight: _medium,
+    fontSize: 14,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _titleMedium = TextStyle(
+    fontWeight: _medium,
+    fontSize: 16,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _titleLarge = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _subtitle2 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 22,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _subtitle1 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _overline = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+
+  static const TextStyle _labelSmall = TextStyle(
+    fontWeight: _medium,
+    fontSize: 11,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _labelMedium = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _labelLarge = TextStyle(
+    fontWeight: _medium,
+    fontSize: 12,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headlineSmall = TextStyle(
+    fontWeight: _medium,
+    fontSize: 24,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headlineMedium = TextStyle(
+    fontWeight: _medium,
+    fontSize: 28,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headlineLarge = TextStyle(
+    fontWeight: _medium,
+    fontSize: 32,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline6 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline5 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline4 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline3 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline2 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _headline1 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _displaySmall = TextStyle(
+    fontWeight: _medium,
+    fontSize: 36,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _displayMedium = TextStyle(
+    fontWeight: _medium,
+    fontSize: 45,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _displayLarge = TextStyle(
+    fontWeight: _medium,
+    fontSize: 57,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _caption = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _button = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _bodyText2 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _bodyText1 = TextStyle(
+    fontWeight: _medium,
+    fontSize: 26,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _bodySmall = TextStyle(
+    fontWeight: _medium,
+    fontSize: 12,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _bodyMedium = TextStyle(
+    fontWeight: _medium,
+    fontSize: 14,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+  static const TextStyle _bodyLarge = TextStyle(
+    fontWeight: _medium,
+    fontSize: 16,
+    height: 1,
+    color: AppDarkModeColors.whiteColor,
+    fontFamily: _fontFamily,
+  );
+}

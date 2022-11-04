243295cee201dcbd9b6962cdd15df93f4f0cc2eb
Hwassef <hassine.wassef@outlook.com>
2022-11-03 15:37:59 -1100

    Initialize text theme on dark mode

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
 import 'package:music_app/app/config/themes/dark_theme/app_list_tile_dark_theme.dart';
+import 'package:music_app/app/config/themes/dark_theme/app_text_dark_theme.dart';
 
 import 'app_bottom_navigation_bar_dark_theme.dart';
 import 'app_bottom_sheet_dark_theme.dart';
@@ -14,5 +15,6 @@ class AppDarkTheme {
     inputDecorationTheme: AppInputDecorationDarkTheme.inputDecorationTheme,
     bottomNavigationBarTheme: AppBottomNavigationBarDarkTheme.bottomNavigationBarThemeData,
     bottomSheetTheme: AppBottomSheetDarkTheme.bottomSheetThemeData,
+    textTheme: AppTexDarkTheme.textTheme,
   );
 }

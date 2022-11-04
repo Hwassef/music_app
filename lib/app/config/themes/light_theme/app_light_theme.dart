3fc35c8232d18516400896a6d2bc9397f3bf39a5
Hwassef <hassine.wassef@outlook.com>
2022-11-03 15:41:11 -1100

    Refactor imports

--- lib/app/config/themes/light_theme/app_light_theme.dart
+++ lib/app/config/themes/light_theme/app_light_theme.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/app/config/colors/app_dark_mode_colors.dart';
-import 'package:music_app/app/config/themes/light_theme/app_text_light_theme.dart';
-
-import 'app_bottom_navigation_bar_light_theme.dart';
-import 'app_bottom_sheet_light_theme.dart';
-import 'app_input_decoration_light_theme.dart';
-import 'app_list_tile_light_theme.dart';
+import 'package:music_app/app/config/themes/light_theme/theme.dart';
 
 class AppLightTheme {
   AppLightTheme._();

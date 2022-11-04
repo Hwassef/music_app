6086bb2d0a6313cde13e77fd37c86d0441a2806d
Hwassef <hassine.wassef@outlook.com>
2022-11-04 01:35:46 -1100

    Add app icon path in assets

--- lib/app/config/images/app_images.dart
+++ lib/app/config/images/app_images.dart
@@ -6,4 +6,5 @@ class AppImages {
   static const homeIcon = '$iconsFolderPath/home_icon.svg';
   static const playlistIcon = '$iconsFolderPath/playlist_icon.svg';
   static const profileIcon = '$iconsFolderPath/profile_icon.svg';
+  static const appIcon = '$iconsFolderPath/app_icon.svg';
 }
